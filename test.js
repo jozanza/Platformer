a4b097b3cf5360224117274f61d91645d2791382
Josiah Savary <hello@jsavary.com>
2020-01-14 10:44:50 -0500

    Update

--- test.js
+++ test.js
@@ -1,267 +1,316 @@
-import * as raylib from './raylib.so'
+import { Button, Color, Canvas, Font, Game, Input, Gfx } from './lib.js'
 
-//------------------------------------------------------------------------------
-// Library
-//------------------------------------------------------------------------------
+const clamp = (min, max, n) => Math.min(max, Math.max(min, n))
 
-class Button {
-  static A = 90 // Z
-  static B = 88 // X
-  static up = 265
-  static down = 264
-  static left = 263
-  static right = 262
-}
+const SPACING = 16
 
-class Color {
-  static black = { r: 0, g: 0, b: 0, a: 255 }
-  static white = { r: 255, g: 255, b: 255, a: 255 }
-  static pink = { r: 255, g: 109, b: 194, a: 255 }
-}
+const EVENT_NEXT_FRAME = Symbol('NextFrameEvent')
+const EVENT_NEW_SCENE = Symbol('NewSceneEvent')
+const EVENT_SET_TAB_INDEX = Symbol('SetTabIndexEvent')
 
-class Window {
-  static title = 'My Game'
-  static width = 512
-  static height = 512
-  static fps = 60
-}
+const SCENE_TITLE = Symbol('TitleScene')
+const SCENE_OPTIONS = Symbol('OptionsScene')
+const SCENE_PLAYER_CREATION = Symbol('PlayerCreationScene')
+const SCENE_OVERWORLD = Symbol('OverworldScene')
 
-class Canvas {
-  static width = 256
-  static height = 256
-}
+const INPUT_TYPE_NUMBER = Symbol('NumberInput')
 
-class Font {
-  static small = null
-  static medium = null
-  static large = null
-  static jumbo = null
-}
-
-class Game {
-  static render(root) {
-    raylib.initWindow(Canvas.width, Canvas.height, Window.title)
-    raylib.setTargetFPS(Window.fps)
-    Font.small = raylib.loadFont('baby.png')
-    root.init()
-    while (!raylib.windowShouldClose()) {
-      // draw
-      raylib.beginDrawing()
-      root.draw()
-      raylib.endDrawing()
-      // update
-      root.update()
-    }
-    root.destroy()
-    raylib.closeWindow()
-  }
-}
-
-class Input {
-  /**
-   * Checks whether a button was pressed this frame
-   *
-   * @param   {Number}  button  button to check
-   *
-   * @return  {Boolean}         was the button pressed
-   */
-  static isButtonPressed(button) {
-    return raylib.isKeyPressed(button)
-  }
-}
-
-class Gfx {
-  /**
-   * Clears the canvas with the given color
-   *
-   * @param   {Color}  color  the clear color
-   */
-  static clear(color = Color.black) {
-    raylib.clearBackground(color)
-  }
-
-  /**
-   * Draws a filled rectangle
-   *
-   * @param   {Number}  x       x position
-   * @param   {Number}  y       y position
-   * @param   {Number}  width   rectangle width
-   * @param   {Number}  height  rectangle height
-   * @param   {Color}   color   rectangle color
-   */
-  static rectfill(x = 0, y = 0, width = 1, height = 1, color = Color.white) {
-    raylib.drawRectangle(x, y, width, height, color)
-  }
-
-  /**
-   * Prints text
-   *
-   * @param   {Font}    font   font to use
-   * @param   {String}  text   text to draw
-   * @param   {Number}  x      x position
-   * @param   {Number}  y      y position
-   * @param   {Color}   color  font color
-   */
-  static print(
-    font = Font.small,
-    text = '',
-    x = 0,
-    y = 0,
-    color = Color.white,
-  ) {
-    switch (font) {
-      case Font.small:
-      default:
-        raylib.drawTextEx(Font.small, text, { x, y }, 4, 1, color)
-    }
+function init() {
+  return {
+    frame: 0,
+    scene: SCENE_TITLE,
+    tabIndex: 0,
+    options: {
+      numberOfRounds: 1,
+      startingLevel: 1,
+      numberOfPlayers: 1,
+    },
+    [SCENE_TITLE]: {
+      blinkRate: 32,
+    },
+    [SCENE_OPTIONS]: {
+      inputs: [
+        [
+          INPUT_TYPE_NUMBER,
+          {
+            tabIndex: 0,
+            label: 'Number of Rounds',
+            defaultValue: 10,
+            value: null,
+            min: 1,
+            max: 100,
+            step: 1,
+          },
+        ],
+        [
+          INPUT_TYPE_NUMBER,
+          {
+            tabIndex: 1,
+            label: 'Starting Level',
+            defaultValue: 1,
+            value: null,
+            min: 1,
+            max: 100,
+            step: 1,
+          },
+        ],
+        [
+          INPUT_TYPE_NUMBER,
+          {
+            tabIndex: 2,
+            label: 'Number of Players',
+            defaultValue: 1,
+            value: null,
+            min: 1,
+            max: 8,
+            step: 1,
+          },
+        ],
+      ],
+    },
+    [SCENE_OVERWORLD]: {
+      players: [],
+    },
   }
 }
 
-class Component {
-  ctx = null
-  children = new Set()
-  setContext(ctx) {
-    this.ctx = ctx
-  }
-  addChild(child) {
-    this.children.add(child)
-    child.ctx = this.ctx
-    child.init()
-  }
-  removeChild(child) {
-    child.destroy()
-    child.ctx = null
-    this.children.delete(child)
-  }
-  init() {}
-  destroy() {
-    for (const child of this.children) {
-      child.destroy?.()
-    }
-  }
-  draw() {
-    for (const child of this.children) {
-      child.draw?.()
-    }
-  }
-  update() {
-    for (const child of this.children) {
-      child.update?.()
-    }
-  }
-}
+function destroy() {}
 
-//------------------------------------------------------------------------------
-// Game
-//------------------------------------------------------------------------------
-
-class Root extends Component {
-  ctx = {
-    scene: null,
-    nextScene: null,
-    tileSize: 16,
-  }
-  init() {
-    console.log('Game init!')
-    const scene = new TitleScene()
-    this.ctx.scene = scene
-    this.addChild(scene)
-  }
-  destroy() {
-    console.log('Game destroy!')
-  }
-  draw() {
-    super.draw()
-  }
-  update() {
-    for (const child of this.children) {
-      if (child !== this.ctx.scene) {
-        this.removeChild(child)
-      }
-    }
-    this.ctx.scene = this.ctx.nextScene ?? this.ctx.scene
-    this.ctx.nextScene = null
-    if (!this.children.size) {
-      this.addChild(this.ctx.scene)
-    }
-    super.update()
-  }
-}
-
-class TitleScene extends Component {
-  frame = 0
-  draw() {
-    Gfx.clear(Color.black)
-    if (this.frame % 32 < 16) {
-      const charWidth = 4
-      const charHeight = 4
-      const charSpacing = 1
+function update(ctx, dispatch) {
+  const { scene, frame } = ctx
+  Gfx.clear(Color.black)
+  if (scene === SCENE_TITLE) {
+    const { blinkRate } = ctx[SCENE_TITLE]
+    if (frame % blinkRate < blinkRate / 2) {
       const msg = 'Press A to Start'
-      const msgWidth = msg.length * (charWidth + charSpacing) - 1
+      const { width, height } = Gfx.measureText(Font.small, msg)
       Gfx.print(
         Font.small,
         msg,
-        Canvas.width / 2 - msgWidth / 2,
-        Canvas.height / 2 - charHeight / 2,
+        Canvas.width / 2 - width / 2,
+        Canvas.height / 2 - height / 2,
         Color.white,
       )
     }
-    super.draw()
-  }
-  update() {
     if (Input.isButtonPressed(Button.A)) {
-      this.ctx.nextScene = new OverworldScene()
+      dispatch(EVENT_NEW_SCENE, SCENE_OPTIONS)
     }
-    this.frame++
-    super.update()
+  } else if (scene === SCENE_OPTIONS) {
+    const { inputs } = ctx[SCENE_OPTIONS]
+    // Draw
+    {
+      let x = 0
+      let y = 0
+      Gfx.print(Font.small, 'Setup!', x, y, Color.white)
+      y += SPACING
+      for (const [type, options] of inputs) {
+        const active = options.tabIndex == ctx.tabIndex
+        if (type == INPUT_TYPE_NUMBER) {
+          const delta = Gfx.numberInput(x, y, active, options)
+          const amount = delta * options.step
+          const currentValue = options.value ?? options.defaultValue
+          options.value = clamp(options.min, options.max, currentValue + amount)
+          y += SPACING
+        }
+      }
+    }
+    // Update
+    {
+      // Cancel
+      if (Input.isButtonPressed(Button.B)) {
+        dispatch(EVENT_NEW_SCENE, SCENE_TITLE)
+      }
+      // Prev tabindex
+      if (Input.isButtonPressed(Button.up)) {
+        const nextTabIndex = Math.max(0, ctx.tabIndex - 1)
+        dispatch(EVENT_SET_TAB_INDEX, nextTabIndex)
+      }
+      // Next tabindex
+      if (Input.isButtonPressed(Button.down)) {
+        const nextTabIndex = Math.min(inputs.length - 1, ctx.tabIndex + 1)
+        dispatch(EVENT_SET_TAB_INDEX, nextTabIndex)
+      }
+    }
+  } else if (scene === SCENE_PLAYER_CREATION) {
+    Gfx.print(Font.small, 'Player Creation!', 0, 0, Color.white)
+  } else if (scene === SCENE_OVERWORLD) {
+    Gfx.print(Font.small, 'Overworld!', 0, 0, Color.white)
+  }
+  dispatch(EVENT_NEXT_FRAME, null)
+}
+
+function subscribe(ctx, [type, data]) {
+  switch (type) {
+    case EVENT_NEXT_FRAME:
+      {
+        ctx.frame++
+      }
+      break
+    case EVENT_NEW_SCENE:
+      {
+        const nextScene = data
+        ctx.scene = nextScene
+        ctx.tabIndex = 0
+      }
+      break
+    case EVENT_SET_TAB_INDEX:
+      {
+        const nextTabIndex = data
+        ctx.tabIndex = nextTabIndex
+      }
+      break
   }
 }
 
-class OverworldScene extends Component {
-  players = []
-  init() {
-    this.players.push(new Player())
-    for (const player of this.players) {
-      this.addChild(player)
-    }
-  }
-  draw() {
-    Gfx.clear(Color.pink)
-    super.draw()
-  }
-  update() {
-    if (Input.isButtonPressed(Button.B)) {
-      this.ctx.nextScene = new TitleScene()
-    }
-    super.update()
-  }
-}
+Game.run(init, destroy, update, subscribe)
 
-class Player extends Component {
-  x = 0
-  y = 0
-  draw() {
-    const { x, y } = this
-    const { tileSize } = this.ctx
-    Gfx.rectfill(x * tileSize, y * tileSize, tileSize, tileSize, Color.black)
-    super.draw()
-  }
-  update() {
-    if (Input.isButtonPressed(Button.up)) {
-      this.y--
-    }
-    if (Input.isButtonPressed(Button.down)) {
-      this.y++
-    }
-    if (Input.isButtonPressed(Button.left)) {
-      this.x--
-    }
-    if (Input.isButtonPressed(Button.right)) {
-      this.x++
-    }
-    super.update()
-  }
-}
+// class Root extends Component {
+//   ctx = {
+//     scene: null,
+//     nextScene: null,
+//     tileSize: 16,
+//     settings = {
+//       numberOfRounds: 1,
+//       startingLevel: 1,
+//       players: [null, null, null, null],
+//     }
+//   }
+//   init() {
+//     console.log('Game init!')
+//     const scene = new TitleScene()
+//     this.ctx.scene = scene
+//     this.addChild(scene)
+//   }
+//   destroy() {
+//     console.log('Game destroy!')
+//   }
+//   draw() {
+//     super.draw()
+//   }
+//   update() {
+//     for (const child of this.children) {
+//       if (child !== this.ctx.scene) {
+//         this.removeChild(child)
+//       }
+//     }
+//     this.ctx.scene = this.ctx.nextScene ?? this.ctx.scene
+//     this.ctx.nextScene = null
+//     if (!this.children.size) {
+//       this.addChild(this.ctx.scene)
+//     }
+//     super.update()
+//   }
+// }
 
-// Start the game
-Game.render(new Root())
+// class TitleScene extends Component {
+//   frame = 0
+//   draw() {
+//     Gfx.clear(Color.black)
+//     if (this.frame % 32 < 16) {
+//       const charWidth = 4
+//       const charHeight = 4
+//       const charSpacing = 1
+//       const msg = 'Press A to Start'
+//       const msgWidth = msg.length * (charWidth + charSpacing) - 1
+//       Gfx.print(
+//         Font.small,
+//         msg,
+//         Canvas.width / 2 - msgWidth / 2,
+//         Canvas.height / 2 - charHeight / 2,
+//         Color.white,
+//       )
+//     }
+//     super.draw()
+//   }
+//   update() {
+//     if (Input.isButtonPressed(Button.A)) {
+//       this.ctx.nextScene = new OverworldScene()
+//     }
+//     this.frame++
+//     super.update()
+//   }
+// }
+
+// class GameSetupScene extends Component {
+//   static NUMBER_OF_ROUNDS = 'numberOfRounds'
+//   static STARTING_LEVEL = 'startingLevel'
+//   static PLAYERS = 'players'
+//   step = 'numberOfRounds'
+//   draw() {
+//     switch(this.step) {
+//       case GameSetupScene.NUMBER_OF_ROUNDS: {
+//         // ...
+//       }; break;
+//       case GameSetupScene.STARTING_LEVEL: {
+//         // ...
+//       }; break;
+//       case GameSetupScene.PLAYERS: {
+//         // ...
+//       }; break;
+//     }
+//     super.draw()
+//   }
+//   update() {
+//     switch(this.step) {
+//       case GameSetupScene.NUMBER_OF_ROUNDS: {
+//         // ...
+//       }; break;
+//       case GameSetupScene.STARTING_LEVEL: {
+//         // ...
+//       }; break;
+//       case GameSetupScene.PLAYERS: {
+//         // ...
+//       }; break;
+//     }
+//     super.update()
+//   }
+// }
+
+// class OverworldScene extends Component {
+//   players = []
+//   init() {
+//     this.players.push(new Player())
+//     for (const player of this.players) {
+//       this.addChild(player)
+//     }
+//   }
+//   draw() {
+//     Gfx.clear(Color.pink)
+//     super.draw()
+//   }
+//   update() {
+//     if (Input.isButtonPressed(Button.B)) {
+//       this.ctx.nextScene = new TitleScene()
+//     }
+//     super.update()
+//   }
+// }
+
+// class Player extends Component {
+//   x = 0
+//   y = 0
+//   draw() {
+//     const { x, y } = this
+//     const { tileSize } = this.ctx
+//     Gfx.rectfill(x * tileSize, y * tileSize, tileSize, tileSize, Color.black)
+//     super.draw()
+//   }
+//   update() {
+//     if (Input.isButtonPressed(Button.up)) {
+//       this.y--
+//     }
+//     if (Input.isButtonPressed(Button.down)) {
+//       this.y++
+//     }
+//     if (Input.isButtonPressed(Button.left)) {
+//       this.x--
+//     }
+//     if (Input.isButtonPressed(Button.right)) {
+//       this.x++
+//     }
+//     super.update()
+//   }
+// }
+
+// // Start the game
+// Game.render(new Root())
